97544a98d7b906011d8cf9a751f1e82e4b840ad8
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-09-06 00:55:16 +0300

    New

--- Ex_7_AdrianGarcia.c
+++ Ex_7_AdrianGarcia.c
@@ -1,10 +1,34 @@
 //
 // Created by Adrian Garcia on 5/9/23.
 //
+#include <stdbool.h>
 #include "stdio.h"
 
+bool read_positive(int *value){
+    int number;
+    do {
+        printf("Enter positive numbers or negative to stop: ");
+        if (scanf("%d", &number) != 1 || number < 0) {
+            while (getchar() != '\n');
+            printf("Invalid input\n");
+            return false;
+        } else {
+            *value = number;
+            return true; // Return the entered number
+        }
+    } while (number <= 0);
+}
 
 int main(){
-    printf("Hello");
-    printf("123");
+    int x;
+    int count = 0;
+    do {
+        bool correct = read_positive(&x);
+        if(correct){
+            printf("You didn't get it right. I have %d euros\n", x*2 + 20);
+        } else{
+            count++;
+        }
+    } while (count < 3);
+    printf("I give up! See you later");
 }
