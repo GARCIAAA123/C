5aeb2fc47dcb8f79d97062f2de1c462c5765de52
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-09-06 12:04:00 +0300

    New

--- Ex_8_AdrianGarcia.c
+++ Ex_8_AdrianGarcia.c
@@ -1,3 +1,22 @@
 //
 // Created by Adrian Garcia on 5/9/23.
 //
+#include <stdlib.h>
+#include <time.h>
+#include "stdio.h"
+
+void print_numbers(const int *array, int count){
+    for (int i = 0; i < count; ++i) {
+        printf("%8d\n",array[i]);
+    }
+}
+
+int main(){
+    int array[15];
+    int count = 15;
+    srand(time(NULL));
+    for (int i =0; i < count; ++i) {
+        array[i] = rand()%100;
+    }
+    print_numbers(array, count);
+}
