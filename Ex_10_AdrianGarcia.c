7690d53a42c350dc385c62fedd5c1bdefaa4f704
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-09-13 11:23:22 +0300

    Ex10

---
+++ Ex_10_AdrianGarcia.c
@@ -0,0 +1,25 @@
+//
+// Created by Adrian Garcia on 13/9/23.
+//
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#define STR_SIZE 80
+
+
+int main(){
+    char text[STR_SIZE];
+    do {
+        printf("Enter a string(Type stop to stop): ");
+        fgets(text, 80, stdin);
+        if (text[strlen(text) - 1] == '\n') {
+            text[strlen(text) - 1] = '\0';
+        }
+        if (strcmp(text, "stop") != 0) {
+            printf("Your input: %s\n", text);
+            printf("Lenght: %lu\n", strlen(text));
+        }
+    } while (strcmp(text, "stop") != 0);
+    printf("Bye!");
+    return 0;
+}
