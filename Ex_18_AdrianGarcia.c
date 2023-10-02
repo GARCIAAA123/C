16ceb654413bfd7f240be42a60c67c6e474f3b68
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-10-02 14:55:44 +0300

    Ex18

---
+++ Ex_18_AdrianGarcia.c
@@ -0,0 +1,28 @@
+//
+// Created by Adrian Garcia on 2/10/23.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+
+int main() {
+    int number = 0;
+    int random_number;
+    srand(time(NULL));
+    while (number >= 0) {
+        printf("Number between 0 and 15 or negative to stop:\n");
+        scanf("%d", &number);
+        if (number < 0) {
+            printf("Program stopped\n");
+        } else if (number <= 15) {
+            random_number = rand();
+            int shifted_number = random_number >> number;
+            shifted_number = shifted_number & 0x3F;
+            printf("Hexa: 0x%02X\n", shifted_number);
+
+        }
+
+    }
+}
