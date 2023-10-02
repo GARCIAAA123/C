f8f079e840d4e23e19a3e65c8ec2bf7472cefd12
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-10-02 14:52:49 +0300

    Ex16

--- Ex_16_AdrianGarcia.c
+++ Ex_16_AdrianGarcia.c
@@ -28,34 +28,38 @@ int main() {
     int number;
     char input[100];
 
-    printf("Enter a number or type 'end' to stop the program:\n");
+    printf("Enter a number or type 'stop' to stop the program:\n");
 
-    while (strcmp(input, "end\n") != 0) {
+    while (strcmp(input, "stop\n") != 0) {
         if (fgets(input, sizeof(input), stdin) == NULL) {
             perror("Input error");
             exit(EXIT_FAILURE);
         }
         if (sscanf(input, "%d", &number) == 1) {
             head = add(number, head);
+            printf("Enter a number or type 'stop' to stop the program:\n");
         }
         if (strcmp(input, "stop\n") == 0) {
             printf("Program stopped!\n");
         } else if (sscanf(input, "%d", &number) != 1) {
             printf("Invalid input. Please enter a number or 'stop'.\n");
         }
+
     }
 
-    printf("Numbers entered:\n");
+
     nnode *current = head;
-    while (current != NULL) {
-        printf("%d\n", current->number);
-        current = current->next;
+    if (current != NULL){
+        printf("Numbers entered:\n");
+        while (current != NULL) {
+            printf("%d\n", current->number);
+            current = current->next;
+        }
+        while (head != NULL) {
+            nnode *temp = head;
+            head = head->next;
+            free(temp);
+        }
     }
-    while (head != NULL) {
-        nnode *temp = head;
-        head = head->next;
-        free(temp);
-    }
-
     return 0;
 }
