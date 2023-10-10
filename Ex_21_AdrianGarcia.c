029c85be199ae6f565d93812a91b135cb653c4e9
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-10-10 19:10:51 +0300

    Ex21

--- Ex_21_AdrianGarcia.c
+++ Ex_21_AdrianGarcia.c
@@ -1,7 +1,11 @@
+//
+// Created by Adrian Garcia on 6/10/23.
+//
+
 #include <stdio.h>
 #include <string.h>
 
-int calculateChecksum(const char *line) {
+int calculate_checksum(const char *line) {
     int checksum = 0;
     for (int i = 1; line[i] != '*' && line[i] != '\0'; i++) {
         checksum ^= line[i];
@@ -10,37 +14,37 @@ int calculateChecksum(const char *line) {
 }
 
 int main() {
-    char filename[256];
+    char input_filename[256];
     printf("Enter the file name: ");
-    scanf("%255s", filename);
+    scanf("%255s", input_filename);
 
-    FILE *file = fopen(filename, "r");
+    FILE *input_file = fopen(input_filename, "r");
 
-    if (file == NULL) {
+    if (input_file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    char line[1024];
-    for (int lineNum = 0; fgets(line, sizeof(line), file); lineNum++) {
-        if (line[0] == '$') {
-            char *asterisk = strchr(line, '*');
-            if (asterisk != NULL) {
-                int expectedChecksum;
-                if (sscanf(asterisk + 1, "%2X", &expectedChecksum) == 1) {
-                    int calculatedChecksum = calculateChecksum(line);
+    char current_line[1024];
+    for (int line_num = 0; fgets(current_line, sizeof(current_line), input_file); line_num++) {
+        if (current_line[0] == '$') {
+            char *asterisk_position = strchr(current_line, '*');
+            if (asterisk_position != NULL) {
+                int expected_checksum;
+                if (sscanf(asterisk_position + 1, "%2X", &expected_checksum) == 1) {
+                    int calculated_checksum = calculate_checksum(current_line);
 
-                    if (calculatedChecksum == expectedChecksum) {
-                        printf("[ OK ] %s", line);
+                    if (calculated_checksum == expected_checksum) {
+                        printf("[ OK ] %s", current_line);
                     } else {
-                        printf("[FAIL] %s", line);
+                        printf("[FAIL] %s", current_line);
                     }
                 }
             }
         }
     }
 
-    fclose(file);
+    fclose(input_file);
 
     return 0;
 }
