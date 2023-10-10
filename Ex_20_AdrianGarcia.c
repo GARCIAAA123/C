91dc5eb4a4532daadd4fe97ba366c7f5eb9a46b1
GARCIAAA123 <112083466+GARCIAAA123@users.noreply.github.com>
2023-10-10 19:47:25 +0300

    Ex21

--- Ex_20_AdrianGarcia.c
+++ Ex_20_AdrianGarcia.c
@@ -6,26 +6,25 @@
 #include <time.h>
 #include "debug.h"
 
-
 int main() {
     srand(time(NULL));
-    int debug_level;
+    int Level_Debug;
 
     printf("Enter debug level (0-4): ");
-    scanf("%d", &debug_level);
+    scanf("%d", &Level_Debug);
 
-    if (debug_level < 0 || debug_level > 4) {
+    if (Level_Debug < 0 || Level_Debug > 4) {
         fprintf(stderr, "Error: Debug level must be in the range 0-4.\n");
         return 1;
     }
 
-    set_debug_level(debug_level);
+    set_debug_level(Level_Debug);
 
     for (int i = 1; i <= 5; i++) {
-        int random_debug_level = rand() % 5;
+        int Random_Level = rand() % 5;
         int numerical_arg = i;
 
-        dprintf(random_debug_level, "This is message %d with debug level %d\n", numerical_arg, random_debug_level);
+        dprintf(Random_Level, "This is message %d with debug level %d\n", numerical_arg, Random_Level);
     }
 
     return 0;
